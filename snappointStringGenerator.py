a049bbbf0188a2d7f9b7d8e5a8ba323ed03fec34
Noisymedal <noisymedal95@gmail.com>
2024-02-26 14:27:38 -0600

    snap points variables
    the snap points size and quantity can now be edited by changing 2 variables rather than the code proper

--- snappointStringGenerator.py
+++ snappointStringGenerator.py
@@ -1,8 +1,10 @@
 output=""
-for i in range(0,100):
-    xval = -0.95+(i*0.019)
-    for j in range(0,100):
-        yval = -0.95+(j*0.019)
+boardSize=100
+spaceSize=0.019
+for i in range(0,boardSize):
+    xval = -0.95+(i*spaceSize)
+    for j in range(0,boardSize:
+        yval = -0.95+(j*spaceSize)
         output+=("\n{\n\t\"Position\": {\n\t \"x\": "+str(xval)+",\n\t \"y\": 0.0,\n\t \"z\": "+str(yval)+"\n\t }\n}," )
 file1 = open('snappoints.txt', 'w')
 file1.write(output)
