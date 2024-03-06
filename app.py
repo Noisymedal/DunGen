a3a3419fb2e2e40b66175e089ee126f6ad488bbb
gabermoore <gabemmoore03@gmail.com>
2024-03-06 14:52:36 -0600

    Add requirements.txt
    Add requirements file and setup instructions in app.py for installing dependencies

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+## SETUP: run the following command in the terminal to install dependencies (flask and matplotlib specifically):
+## -m pip install -r requirements.txt
+## run the web app by typing the following command in the terminal:
+## python -m flask --app app run
+## the terminal will output the localhost address it is running on
+
 from flask import Flask, render_template
 import generator
 
