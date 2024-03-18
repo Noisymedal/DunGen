6cea5f9c2cbc3d686e0c2ccefde06bdf757b405e
Mitchell Ross <mross3143@gmail.com>
2024-03-17 22:24:36 -0500

    front-end updates

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def generate_dungeon():
     generator.main()
-    return render_template("template.html")
+    return render_template("generator.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
